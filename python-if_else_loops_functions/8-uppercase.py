3e5dad15c0c3108823bf4194a7d1fefd865df4aa
IverSutizal <iversutizal@gmail.com>
2022-09-11 23:44:52 -0500

    task

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            upper = ord(i) - 32
+        else:
+            upper = ord(i)
+        print("{:s}".format(chr(upper)), end="")
+    print("")
