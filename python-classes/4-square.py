276c73a379eb2124431495015bc433480bdeaf68
IverSutizal <iversutizal@gmail.com>
2022-09-25 22:16:52 -0500

    task

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+Super simple Square Module.
+
+"""
+
+
 class Square:
-    """ A class that defines a square by its size
+    """
+    Only does one thing: Gives you its area.
     """
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -13,23 +18,16 @@ class Square:
             self.__size = size
 
     def area(self):
-        """ Method that returns the square are of the object
-        """
         return (self.__size ** 2)
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
