198bfd61049312ca41cb23d36540a5a43817a02f
IverSutizal <iversutizal@gmail.com>
2022-10-02 23:44:46 -0500

    task

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 """
-Module 10-square
-Contains parent class BaseGeometry
-with public instance method area and integer_validation
-Contains subclass Rectangle
-with instantiation of private attributes width and height, validated by parent,
-extends parent's area method and prints with __str__
-Contains subclass Square
-with instantiation of private attribute size, validated by superclass
+Contains the definition of the class Square.
 """
-
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """inherits from Rectangle, who inherits from BaseGeometry
-    methods:
-        __init__(self, size)
-    """
+    """Definition of class Square that inherits from class Rectangle"""
+
     def __init__(self, size):
-        """initializes size
-        Args:
-            size (int): private
-        """
-        self.integer_validator("size", size)
+        """Initialise an instance of the class Square"""
+        super().integer_validator("size", size)
         super().__init__(size, size)
-        self.__size =
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
