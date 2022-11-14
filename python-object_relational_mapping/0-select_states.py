536ca68a1f646d4739eae088d273c022ad69846c
IverSutizal <iversutizal@gmail.com>
2022-11-13 23:58:29 -0500

    task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""This module lists all states from the database hbtn_0e_0_usa
-   Created on Saturday, November 12, 2022
-   @author: DaisyG Chipana Lapa
-"""
+''' Script to retrieve data from a database '''
+import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                         host='localhost', port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in cur.fetchall()]
     cur.close()
-    conn.close()
+    db.close()
