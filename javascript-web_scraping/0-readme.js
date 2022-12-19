9b35fe3ffdcf2f88b6c30f59d70e2cc121102d8d
IverSutizal <iversutizal@gmail.com>
2022-12-18 23:16:48 -0500

    task

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-
-let filename = process.argv[2];
 const fs = require('fs');
-fs.readFile(filename, 'utf8', function (err, data) {
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
